42dffe729c04dc8a7f14b3753b07cbf4ad57400e
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-13 13:11:18 -0300

    Criando as rotas em pasta separada

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const linkRouters = require('./routers/LinkRouters');
 const PORT = 3000;
 
 mongoose.connect('mongodb://localhost/newLinks', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -9,7 +10,7 @@ let db = mongoose.connection
 db.on('error', () => console.log('houve um erro')); 
 db.once('open', () => console.log('Banco carregado'));
 
-app.use('/', (req, res) => res.send('Hello World'));
+app.use('/', linkRouters);
 
 app.listen(PORT, () => {
     try{
