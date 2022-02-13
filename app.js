4e36ba19980aa43917f1cf0bdb45d793350ef5cd
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-13 12:58:25 -0300

    Conectando com o MongoDB através do mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost/newLinks', { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = mongoose.connection
+db.on('error', () => console.log('houve um erro')); 
+db.once('open', () => console.log('Banco carregado'));
 
 app.use('/', (req, res) => {res.send('Hello World') });
 
