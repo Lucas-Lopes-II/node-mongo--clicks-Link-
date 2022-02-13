67cbb5d9cb0e29ecf62fd89b44c5c5518abefec2
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-13 13:19:46 -0300

    Criando controller da aplicação. Primeira função: redirecionar para o link

---
+++ controllers/linkController.js
@@ -0,0 +1,17 @@
+const Link = require('../models/Link');
+
+const redirect = async (req, res, next) => {
+    let title = req.params.title;
+    try {
+        let doc = await Link.findOne({ title });
+        if(doc){
+            res.redrect(doc.url);
+        }else{
+            next();
+        }
+    }catch(err){
+        res,send(err.message);
+    }
+};
+
+module.exports = { redirect };
