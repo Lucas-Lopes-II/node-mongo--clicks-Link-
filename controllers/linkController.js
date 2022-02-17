280c765d5a4f8c4dfc1e101787c4f4f05ebfab91
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-17 15:32:12 -0300

    Criação da função de deletar link do banco de dados

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -3,9 +3,9 @@ const Link = require('../models/Link');
 const redirect = async (req, res, next) => {
     let title = req.params.title;
     try{
-        let doc = await Link.findOne({ title });
+        let doc = await Link.findOneAndUpdate({ title }, { $inc:{ clicks: 1 } });
         if(doc){
-            res.redrect(doc.url);
+            res.redirect(doc.url);
         }else{
             next();
         };
@@ -33,4 +33,15 @@ const allLinks = async (req, res) => {
     };
 };
 
-module.exports = { redirect, addLink, allLinks };
+const deleteLink = async (req, res) => {
+    let title = req.params.id;
+    if(!id){ id = req.body.id };
+    try{
+        await Link.findByIdAndDelete(id);
+        res.send(id);
+    }catch(err){
+        res.status(404).send(err);
+    };
+};
+
+module.exports = { redirect, addLink, allLinks, deleteLink };
