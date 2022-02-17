8fd0a398f6a84768875bec95af4f3a014701f5f5
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2022-02-17 15:37:52 -0300

    Criação de rotas de delete

--- routers/LinkRouters.js
+++ routers/LinkRouters.js
@@ -8,4 +8,7 @@ router.get('/add', (req, res) => res.render('add', { err: false, body: {} }));
 
 router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
 
+router.delete('/:id', linkController.deleteLink);
+router.delete('/:id', express.json(), linkController.deleteLink);
+
 module.exports = router;
